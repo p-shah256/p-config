06ec170ee622f04a825e5af865e7490aa2ad4961
Pranchal Shah <shah256@meta.com>
2025-12-22 15:26:46 -0800

    colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -970,21 +970,6 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        style = 'storm', -- storm is brighter than night
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
-    end,
   },
 
   -- Highlight todo, notes, etc in comments
--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -171,5 +171,8 @@ return {
 
   {
     'sainnhe/gruvbox-material',
+    config = function()
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
   },
 }
