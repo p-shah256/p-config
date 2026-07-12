0617f074e47218155f5fe63474dd01ad7cd89910
Pranchal Shah <pranchx@gmail.com>
2026-07-12 12:25:17 -0400

    add pgformatter

--- nvim/lua/format.lua
+++ nvim/lua/format.lua
@@ -9,23 +9,26 @@ M.fmt_ft = {
 
 -- In-place formatters (modify file directly, then reload)
 M.inplace_fmt_ft = {
-	-- configerator = { "arc", "f" },
 	python = { "ruff", "format" },
+	sql = { "pg_format", "--inplace" }, -- gotta love this, no dependencies, written in perl :O
 }
 
--- NEOVIM:
--- crash course on nvim threading/scheduling
--- two loops:
--- A) main loop - check for user input;; run any scheduled callbacks;; redraw screen;; repeat
--- B) libuv with its set of thread pool to do all the work in background.
---
--- When you call vim.system(), libuv spawns a new process with fmtr and monitors the output with its thread.
--- while vim.system() returns immideatly (non blcking).
--- on proc exit, it will trigger the registered callback
---
--- this means that all the editor operations (vim.cmd and vim.api.*) should be run in thread A to avoid race conditions b/w A and B.
--- when the callback fires, we want all the editor ops to run
--- on thread A. vim.schedule will put that work on thread A.
+--[[
+NEOVIM:
+-----------------------------------------------------
+crash course on nvim threading/scheduling. two loops:
+A) main loop - check for user input;; run any scheduled callbacks;; redraw screen;; repeat
+B) libuv with its set of thread pool to do all the work in background.
+
+When you call vim.system(), libuv spawns a new process with fmtr and monitors
+the output with its thread. while vim.system() returns immideatly (non
+blcking). on proc exit, it will trigger the registered callback
+
+this means that all the editor operations (vim.cmd and vim.api.*) should be run
+in thread A to avoid race conditions b/w A and B. when the callback fires, we
+want all the editor ops to run on thread A. vim.schedule will put that work on
+thread A.
+--]]
 
 local function inplace_fmt(fmtr)
 	if vim.bo.modified then
